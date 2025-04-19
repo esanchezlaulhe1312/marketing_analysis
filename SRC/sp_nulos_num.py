8524ed61332b65057757dc3e6102d5ad86a058bf
esanchezlaulhe88 <esanchezlaulhe@gmail.com>
2025-04-19 03:05:00 +0200

    gestión nulos

--- SRC/sp_nulos_num.py
+++ SRC/sp_nulos_num.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 def calcular_solo_col_null(dataframe,umbral=10):
   columns_with_nulls=dataframe.columns[dataframe.isnull().any()]
   null_columns_info=pd.DataFrame(
@@ -9,6 +11,15 @@ def calcular_solo_col_null(dataframe,umbral=10):
      "Null%":[((dataframe[col].isnull().sum()/dataframe.shape[0])*100) for col in columns_with_nulls]}
   )
   display(null_columns_info)
-  high_null_cols=null_columns_info[null_columns_info['Null%']>umbral]['Column'].tolist()
-  low_null_cols=null_columns_info[null_columns_info['Null%']<=umbral]['Column'].tolist()
+  low_null_cols=null_columns_info[null_columns_info['Null%']>umbral]['Column'].tolist()
+  high_null_cols=null_columns_info[null_columns_info['Null%']<=umbral]['Column'].tolist()
   return high_null_cols,low_null_cols
+
+def imputar_iterative(dataframe,lista_columnas):
+  iter_imputer=IterativeImputer(max_iter=50,
+                              random_state=42)
+  data_imputed=iter_imputer.fit_transform(dataframe[lista_columnas])
+  new_col=[col for col in lista_columnas]
+  dataframe[new_col]=data_imputed
+  display(dataframe[new_col].describe().T)
+  return dataframe,new_col
